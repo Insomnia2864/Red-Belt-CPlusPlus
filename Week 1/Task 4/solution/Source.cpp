f5cbb14ae1c961ee9381021ca85c5138fd4130f3
Insomnia2864 <52221605+Insomnia2864@users.noreply.github.com>
2020-01-01 21:36:36 +0300

    Add files via upload

---
+++ Week 1/Task 4/solution/Source.cpp
@@ -0,0 +1,14 @@
+#include <ostream>
+
+#include "test_runner.h"
+
+#define PRINT_VALUES(out, x, y) out << (x) << '\n' << (y) << '\n'            
+
+int main() {
+    TestRunner tr;
+    tr.RunTest([] {
+        std::ostringstream output;
+        PRINT_VALUES(output, 5, "red belt");
+        ASSERT_EQUAL(output.str(), "5\nred belt\n");
+        }, "PRINT_VALUES usage example");
+}
