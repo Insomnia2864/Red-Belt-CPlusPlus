d0cda6811ae19882f0e16c2fae7481ec6b7121a8
Insomnia2864 <52221605+Insomnia2864@users.noreply.github.com>
2020-01-02 01:22:26 +0300

    Add files via upload

---
+++ Week 1/Task 7/solution/Source.cpp
@@ -0,0 +1,164 @@
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+
+template <typename T>
+class Deque
+{
+public:
+	Deque() = default;
+
+	bool Empty() const;
+
+	std::size_t Size() const;
+
+	T& operator[](std::size_t index);
+	const T& operator[](std::size_t index) const;
+
+	const T& At(std::size_t index) const;
+	T& At(std::size_t index);
+
+	T& Front();
+	const T& Front() const;
+
+	T&Back();
+	const T& Back() const;
+
+	void PushFront(const T& elem);
+
+	void PushBack(const T& elem);
+
+private:
+	std::vector<T> begin_;
+	std::vector<T> end_;
+};
+
+template <typename T>
+bool Deque<T>::Empty() const
+{
+	return begin_.empty() && end_.empty();
+}
+
+template <typename T>
+std::size_t Deque<T>::Size() const
+{
+	return begin_.size() + end_.size();
+}
+
+template <typename T>
+T& Deque<T>::operator[](std::size_t index)
+{
+	if (index >= this->Size())
+	{
+		throw std::out_of_range("index > size");
+	}
+
+	if (index < begin_.size())
+	{
+		return begin_[begin_.size() - index - 1];
+	}
+
+	return end_[index - begin_.size()];
+}
+
+template <typename T>
+const T& Deque<T>::operator[](std::size_t index) const
+{
+	if (index >= this->Size())
+	{
+		throw std::out_of_range("index > size");
+	}
+
+	if (index < begin_.size())
+	{
+		return begin_[begin_.size() - index - 1];
+	}
+
+	return end_[index - begin_.size()];
+}
+
+template <typename T>
+const T& Deque<T>::At(std::size_t index) const
+{
+	if (index >= this->Size())
+	{
+		throw std::out_of_range("index > size");
+	}
+
+	if (index < begin_.size())
+	{
+		return begin_[begin_.size() - index - 1];
+	}
+
+	return end_[index - begin_.size()];
+}
+
+template <typename T>
+T& Deque<T>::At(std::size_t index)
+{
+	if (index >= this->Size())
+	{
+		throw std::out_of_range("index > size");
+	}
+
+	if (index < begin_.size())
+	{
+		return begin_[begin_.size() - index - 1];
+	}
+
+	return end_[index - begin_.size()];
+}
+
+
+template <typename T>
+T& Deque<T>::Front()
+{
+	if (!begin_.empty())
+	{
+		return begin_.back();
+	}
+	return end_.front();
+}
+
+
+template <typename T>
+const T& Deque<T>::Front() const
+{
+	if (!begin_.empty())
+	{
+		return begin_.back();
+	}
+	return end_.front();
+}
+
+template <typename T>
+T& Deque<T>::Back()
+{
+	if (!end_.empty())
+	{
+		return end_.back();
+	}
+	return begin_.front();
+}
+
+template <typename T>
+const T& Deque<T>::Back() const
+{
+	if (!end_.empty())
+	{
+		return end_.back();
+	}
+	return begin_.front();
+}
+
+template <typename T>
+void Deque<T>::PushFront(const T& elem)
+{
+	begin_.push_back(elem);
+}
+
+template <typename T>
+void Deque<T>::PushBack(const T& elem)
+{
+	end_.push_back(elem);
+}
