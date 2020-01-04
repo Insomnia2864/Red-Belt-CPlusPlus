9dce8a56ed441b2c76e625d6a6d439b7c33a6daa
Insomnia2864 <52221605+Insomnia2864@users.noreply.github.com>
2020-01-05 01:19:08 +0300

    Add files via upload

---
+++ Week 3/Task 3/solution/simple_vector.h
@@ -0,0 +1,107 @@
+#pragma once
+
+#include <cstdlib>
+
+template <typename T>
+class SimpleVector 
+{
+public:
+    SimpleVector();
+    explicit SimpleVector(size_t size);
+    ~SimpleVector();
+
+    T& operator[](size_t index);
+
+    T* begin();
+    T* end();
+
+    size_t Size() const;
+    size_t Capacity() const;
+    void PushBack(const T& value);
+
+private:
+    std::size_t capacity_;
+    std::size_t size_;
+    T* elements_;
+};
+
+template <typename T>
+SimpleVector<T>::SimpleVector() :
+    capacity_(0),
+    size_(0),
+    elements_(nullptr)
+{}
+
+template <typename T>
+SimpleVector<T>::SimpleVector(size_t size) :
+    capacity_(size),
+    size_(size),
+    elements_(new T[capacity_])
+{}
+
+template <typename T>
+SimpleVector<T>::~SimpleVector()
+{
+    delete[] elements_;
+}
+
+template <typename T>
+T& SimpleVector<T>::operator[](size_t index)
+{
+    return elements_[index];
+}
+
+template <typename T>
+T* SimpleVector<T>::begin()
+{
+    return elements_;
+}
+
+template <typename T>
+T* SimpleVector<T>::end()
+{
+    return elements_ + size_;
+}
+
+template <typename T>
+size_t SimpleVector<T>::Size() const
+{
+    return size_;
+}
+
+template <typename T>
+size_t SimpleVector<T>::Capacity() const
+{
+    return capacity_;
+}
+
+template <typename T>
+void SimpleVector<T>::PushBack(const T& value)
+{
+    if (capacity_ == size_)
+    {
+        T* tmp;
+        if (capacity_ == 0)
+        {
+            capacity_ = 1;
+            tmp = new T[capacity_];
+        }
+        else
+        {
+            capacity_ *= 2;
+            tmp = new T[capacity_];
+        }
+
+        for (size_t i = 0; i < size_; i++)
+        {
+            tmp[i] = elements_[i];
+        }
+        tmp[size_++] = value;
+        delete[] elements_;
+        elements_ = tmp;
+        return;
+    }
+
+    elements_[size_++] = value;
+
+}
